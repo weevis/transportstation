277b60ca2460ea0f9f55a098c2c90b680419fd2f
Kevin Roberts <kevin@nutech.com>
2016-07-22 12:21:15 -0700

    DB

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
