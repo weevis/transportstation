b531b031d0f5011db5f09d00b8ad3e51467561e9
Kevin Roberts <kevin@nutech.com>
2016-07-25 11:01:21 -0700

    Verbage, upload torrent, blah blah

--- nzb/forms.py
+++ nzb/forms.py
@@ -2,4 +2,4 @@ from flask_wtf import Form
 from flask_wtf.file import FileField, FileAllowed
 
 class uploadNZBForm(Form):
-	filename = FileField('nzb', validators=[FileAllowed('nzb', 'NZB Files Only!')])
+	filename = FileField('nzb', validators=[FileAllowed(['nzb', 'torrent', 'magnet'], 'NZB Files Only!')])
